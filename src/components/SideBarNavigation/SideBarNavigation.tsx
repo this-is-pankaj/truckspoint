147c03b2326a5fec8c5927f57620018e0e936aae
Pankaj Gupta <pankajguptanitdgp4@gmail.com>
2025-05-10 14:24:38 +0200

    - fixed issue with active tenant on sidebar

--- src/components/SideBarNavigation/SideBarNavigation.tsx
+++ src/components/SideBarNavigation/SideBarNavigation.tsx
@@ -152,13 +152,13 @@ export function AppSidebar({ ...props }: ComponentProps<typeof Sidebar>) {
   useEffect(() => {
     const fetchData = async () => {
       const { status, data } = await getAllTenantsAction()
-      if(status !== 200) {
+      if(status !== 200 || !data) {
         setTenants({all: [], active: null})
         return
       }
       const activeTenantId = await getActiveTenantId()
       const activeTenant = data.find((tenant: TenantSummary) => tenant.tenantId === activeTenantId)
-      setTenants({all: data, active: activeTenant})
+      setTenants({all: data, active: activeTenant || data[0]})
     }
     fetchData()
   }, [])
